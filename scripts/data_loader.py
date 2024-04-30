0260634b51327a8126b6e8751fa7d546d36fbb86
Abby DeFranco <adefr011@gmail.com>
2024-04-29 22:47:26 -0400

    last update

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -4,8 +4,19 @@ import numpy as np
 import shutil, os
 from pydub import AudioSegment
 
-train=pd.read_csv('C:\\Users\\mateo\\Desktop\\ABBY GOES HERE\\bees\\all_data_updated.csv')
+train=pd.read_csv('C:\\Users\\mateo\\Desktop\\ABBY GOES HERE\\bees\\train.csv')
 
+# for i in range(0,7):
+#     x = []
+#     a = train[train["hive number"]==i]
+#     for j in range(0,4):
+#         x.append(a[a["queen status"]==j].shape[0])
+#     x = np.array(x)
+#     print(x/x.sum())
+#     print(x.sum())
+
+for i in range(0,5):
+    print(train[train["queen status"]==i].shape)
 import ipdb; ipdb.set_trace()
 
 # Id=[]
@@ -73,20 +84,3 @@ print("Step 4 complete...")
 
 
 
-def create_sol():
-    sol=pd.DataFrame()
-    sol=sol.assign(filename=list(set(file['file name'].unique().tolist())-set(train['file name'].unique().tolist())))
-
-    newpath = r'./sol' 
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)
-
-    print(len(list(sol['filename'].unique())))
-    x = 0
-    for i in list(sol['filename'].unique()): # Image Id
-            if(x%500==0):
-                print(x)
-            get_image = os.path.join(base, i) # Path to Images 
-            move_image_to_cat = shutil.copy(get_image, newpath)
-            x+=1
-    print("Step 2 complete...")
